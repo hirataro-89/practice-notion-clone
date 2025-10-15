e5e2a12715b30d386db59f57946c22372248487f
yutaro hirabayashi <hirataro.yu@gmail.com>
2025-10-15 10:50:54 +0900

    Add Supabase dependencies and implement routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import Layout from "./Layout"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import NoteDetail from "./pages/NoteDetail"
+
 function App() {
   return (
-    <div>Hello World</div>
-  )
+    <BrowserRouter>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/note/:id" element={<NoteDetail />} />
+          </Route>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App
