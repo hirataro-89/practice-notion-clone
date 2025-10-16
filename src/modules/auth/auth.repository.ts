e2fddec5ed39e0425596d1fb687173d1936aee6f
yutaro hirabayashi <hirataro.yu@gmail.com>
2025-10-16 09:28:03 +0900

    add: サインアップとサインイン機能

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,21 +1,34 @@
-import { supabase } from "@/lib/supabase"
+import { supabase } from "@/lib/supabase";
 
 export const authRepository = {
-  async signup (name: string, email: string, password: string) {
-    const { data, error } = await supabase.auth.signUp({
+	async signup(name: string, email: string, password: string) {
+		const { data, error } = await supabase.auth.signUp({
 			// メモ：supabaseのユーザー登録にユーザー名を追加。optionsに自由に設定が可能
 			email,
 			password,
 			options: { data: { name } },
 		});
-    if (error != null || data.user == null ) throw new Error(error?.message);
+		if (error != null || data.user == null) throw new Error(error?.message);
 
-    return {
-      ...data.user,
-      userName: data.user.user_metadata.name,
-      // メモ：新規optionで追加したnameは
-      // data.user.user_metadata.name で取得できる。
-      // ただ、これを毎回記述するのは冗長なので、userNameとして返すようにしている
-    };
-  },
+		return {
+			...data.user,
+			userName: data.user.user_metadata.name,
+			// メモ：新規optionで追加したnameは
+			// data.user.user_metadata.name で取得できる。
+			// ただ、これを毎回記述するのは冗長なので、userNameとして返すようにしている
+		};
+	},
+
+	async signin(email: string, password: string) {
+		const { data, error } = await supabase.auth.signInWithPassword({
+			email,
+			password,
+		});
+		if (error != null || data.user == null) throw new Error(error?.message);
+
+		return {
+			...data.user,
+			userName: data.user.user_metadata.name,
+		};
+	},
 };
