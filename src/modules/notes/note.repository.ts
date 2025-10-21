0c9ee30396f3f7381b5835885ef10627a05c9e47
yutaro hirabayashi <hirataro.yu@gmail.com>
2025-10-21 16:13:19 +0900

    feat: implement note fetching and update NoteList component to display notes

--- src/modules/notes/note.repository.ts
+++ src/modules/notes/note.repository.ts
@@ -1,17 +1,32 @@
 import { supabase } from "@/lib/supabase";
 
 export const noteRepository = {
-  async create (userId: string, params: {title?: string; parentId?: number }) {
-    const { data, error } = await supabase.from("notes").insert([
-			{
-				user_id: userId,
-				title: params.title,
-				parent_document: params.parentId,
-			},
-		])
-    .select()
-    .single();
-    if (error != null) throw new Error(error.message);
-    return data;
-  }
-}
+	async create(userId: string, params: { title?: string; parentId?: number }) {
+		const { data, error } = await supabase
+			.from("notes")
+			.insert([
+				{
+					user_id: userId,
+					title: params.title,
+					parent_document: params.parentId,
+				},
+			])
+			.select()
+			.single();
+		if (error != null) throw new Error(error.message);
+		return data;
+	},
+
+	async find(userId: string, pareentDocumentId?: number) {
+		const query = supabase
+			.from("notes")
+			.select()
+			.eq("user_id", userId)
+			.order("created_at", { ascending: false });
+		const { data } =
+			pareentDocumentId != null
+				? await query.eq("parent_document", pareentDocumentId)
+				: await query.is("parent_document", null);
+		return data;
+	},
+};
