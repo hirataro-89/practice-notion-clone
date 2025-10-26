a230b7d30383fd2e6c6743fc51f5efb49ccb3369
yutaro hirabayashi <hirataro.yu@gmail.com>
2025-10-26 12:02:07 +0900

    feature: note取得メソッド

--- src/pages/NoteDetail.tsx
+++ src/pages/NoteDetail.tsx
@@ -1,6 +1,37 @@
 import { TitleInput } from '@/components/Toolbar';
+import { useCurrentUserStore } from '@/modules/auth/current-user.state';
+import { noteRepository } from '@/modules/notes/note.repository';
+import { useNoteStore } from '@/modules/notes/note.state';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const NoteDetail = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const params = useParams();
+  const id = parseInt(params.id!);
+  const {currentUser} = useCurrentUserStore();
+  const noteStore = useNoteStore();
+  const note = noteStore.getOne(id);
+
+  useEffect(() => {
+    fetchOne();
+  }, [id])
+
+  const fetchOne = async () => {
+    setIsLoading(true);
+    const note = await noteRepository.findOne(currentUser!.id, id)
+    if (note == null) return;
+    noteStore.set([note]);
+    setIsLoading(false);
+  }
+
+
+  if (isLoading) return <div>Loading...</div>
+
+  if (note == null) return <div>Note is not found.</div>
+
+  console.log(note);
+
   return (
     <div className="pb-40 pt-20">
       <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
